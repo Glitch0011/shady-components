12fe0794725ec2c19473bf374e08c0f3f91b263b
Tom Bonner <tom_bonner@hotmail.com>
2017-12-03 21:16:06 +0000

    Added example of updating live render on updated attributes

---
+++ Examples/Updating Text/UpdatingTextElement/UpdatingTextElement.js
@@ -0,0 +1,14 @@
+import {ShadyElement} from "/shady.js"
+
+export class UpdatingTextElement extends ShadyElement {
+    connectedCallback() {
+        super.connectedCallback();
+
+        setInterval(() =>
+        {
+            this.DataText = (parseInt(this.DataText) + 1).toString();
+        }, this.DataInterval);
+    }
+}
+
+ShadyElement.Register(UpdatingTextElement, "/Examples/Updating Text/UpdatingTextElement/", {css: true});
