5ad800dedd421fca3e8e9d905170699a5c64b625
Tom Bonner <tom@bonner.is>
2017-12-30 16:21:15 +0000

    Set the data-val if it doesn't already exist

--- shady.js
+++ shady.js
@@ -33,6 +33,8 @@ export default class ShadyElement extends HTMLElement {
                 return;
             }
         }
+
+        this.dataset[val.toLowerCase()] = data;
     }
 
     async connectedCallback() {
